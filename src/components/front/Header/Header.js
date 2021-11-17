eaa93635a0dfa7d57e753a596cff468c1a22a643
vijay <vijayramreddy990@gmail.com>
2021-11-18 01:26:09 +0530

    static header

--- src/components/front/Header/Header.js
+++ src/components/front/Header/Header.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="header">
+      <div>
+        <h1>
+          <Link to="/" className="logo">
+            Electronics Shop
+          </Link>
+        </h1>
+      </div>
+      <div className="header-links">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <Link to="/cart" className="cart">
+              <i class="fas fa-shopping-cart" />
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
 };
 
 export default Header;

ce6d23ddda916b92973ad4a493712f8e3e484bd4
vijay <vijayramreddy990@gmail.com>
2021-11-20 03:23:54 +0530

    pricing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import data from "./components/back/Data/Data";
 import Header from "./components/front/Header/Header";
@@ -7,11 +7,56 @@ import VariousRoutes from "./components/front/Routes/Routes";
 
 const App = () => {
   const { productItems } = data;
+  const [cartItems, setCartItems] = useState([]);
+
+  const handleAddProduct = (product) => {
+    const ProductExist = cartItems.find((item) => item.id === productItems.id);
+    if (ProductExist) {
+      setCartItems(
+        cartItems.map((item) =>
+          item.id === product.id
+            ? {
+                ...ProductExist,
+                quantity: ProductExist.quantity + 1,
+              }
+            : item
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, quantity: 1 }]);
+    }
+  };
+
+  const handleRemoveProduct = (product) => {
+    const ProductExist = cartItems.find((item) => item.id === product.id);
+    if (ProductExist.quantity === 1) {
+      setCartItems(cartItems.filter((item) => item.id !== product.id));
+    } else {
+      setCartItems(
+        cartItems.map((item) =>
+          item.id === product.id
+            ? { ...ProductExist, quantity: ProductExist.quantity - 1 }
+            : item
+        )
+      );
+    }
+  };
+
+  const handleCartClearance = () => {
+    setCartItems([]);
+  };
+
   return (
     <div>
       <Router>
         <Header />
-        <VariousRoutes productItems={productItems} />
+        <VariousRoutes
+          productItems={productItems}
+          cartItems={cartItems}
+          handleAddProduct={handleAddProduct}
+          handleRemoveProduct={handleRemoveProduct}
+          handleCartClearance={handleCartClearance}
+        />
       </Router>
     </div>
   );
--- src/components/front/Routes/Routes.js
+++ src/components/front/Routes/Routes.js
@@ -4,13 +4,38 @@ import Products from "../Products/Products";
 import Signup from "../Signup/Signup";
 import Cart from "../Cart/Cart";
 
-const VariousRoutes = ({ productItems }) => {
+const VariousRoutes = ({
+  productItems,
+  cartItems,
+  handleAddProduct,
+  handleRemoveProduct,
+  handleCartClearance,
+}) => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Products productItems={productItems} />} />
+        <Route
+          path="/"
+          element={
+            <Products
+              productItems={productItems}
+              handleAddProduct={handleAddProduct}
+            />
+          }
+        />
         <Route path="/signup" exact element={<Signup />} />
-        <Route path="/cart" exact element={<Cart />} />
+        <Route
+          path="/cart"
+          exact
+          element={
+            <Cart
+              cartItems={cartItems}
+              handleAddProduct={handleAddProduct}
+              handleRemoveProduct={handleRemoveProduct}
+              handleCartClearance={handleCartClearance}
+            />
+          }
+        />
       </Routes>
     </div>
   );

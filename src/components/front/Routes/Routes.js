70ec108b58c6630fde2826ab28b5b66e63c8a1da
vijay <vijayramreddy990@gmail.com>
2021-11-20 01:43:08 +0530

    signup mock

--- src/components/front/Routes/Routes.js
+++ src/components/front/Routes/Routes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Products from "../Products/Products";
+import Signup from "../Signup/Signup";
 
 const VariousRoutes = ({ productItems }) => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Products productItems={productItems} />} />
+        <Route path="/signup" exact element={<Signup />} />
       </Routes>
     </div>
   );
---
+++ src/components/front/Signup/Signup.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Signup.css";
+const Signup = () => {
+  return (
+    <div className="signup">
+      <div>signup</div>
+    </div>
+  );
+};
+export default Signup;

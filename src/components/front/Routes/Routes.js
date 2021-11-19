e6c3dc87bd0204b84679ef72bd15b8ad01655b03
vijay <vijayramreddy990@gmail.com>
2021-11-20 01:49:01 +0530

    cart

--- src/components/front/Routes/Routes.js
+++ src/components/front/Routes/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Products from "../Products/Products";
 import Signup from "../Signup/Signup";
+import Cart from "../Cart/Cart";
 
 const VariousRoutes = ({ productItems }) => {
   return (
@@ -9,6 +10,7 @@ const VariousRoutes = ({ productItems }) => {
       <Routes>
         <Route path="/" element={<Products productItems={productItems} />} />
         <Route path="/signup" exact element={<Signup />} />
+        <Route path="/cart" exact element={<Cart />} />
       </Routes>
     </div>
   );
